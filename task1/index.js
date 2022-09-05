320f1f6ac52c1dc60d2ea30867d1d108ee0492a3
Arogundade-Aanuoluwa <110118941+Arogundade-Aanuoluwa@users.noreply.github.com>
2022-09-05 20:15:15 +0100

    Add files via upload

---
+++ task1/index.js
@@ -0,0 +1,17 @@
+
+//START
+//DECLARE A FUNCTION
+function indexOf(arr){
+    //create an if statemnt
+    if(arr[0] === arr[arr.length -1]){
+        //RETURN THE FIRST VARIABLE SUM TO STORE
+        return true
+        // CREATE AN ELSE STATEMENT
+    } else {
+        //RETURN SECOND VARIABLE SUM TO STORE
+        return false
+    }
+}
+// PRINT THE VALUE OF THE VARIABLE & DECLARE THE VARIABLE IN THE PRINT
+console.log(indexOf([23, 23, 45, 67, 76, 23])); 
+//END
